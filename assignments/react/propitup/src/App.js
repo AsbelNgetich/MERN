af68ce135eba82d9537dcab8539e3eeedd270a85
Asbel <asbeln01@yahoo.com>
2021-07-08 14:37:24 -0500

    props assigniment

---
+++ assignments/react/propitup/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import PersonalCard from './components/PersonalCard';
+
+
+function App() {
+  return (
+    <div className="App">
+        <PersonalCard className="personalCard" firstName="Jane" lastName="Doe" age={45} hairColor="green"/>
+        <PersonalCard className="personalCard" firstName="John" lastName="Smith" age={32} hairColor="purple"/>
+        <PersonalCard className="personalCard" firstName="Millard" lastName="Fillmore" age={50} hairColor="black"/>
+        </div>
+  );
+}
+
+export default App;

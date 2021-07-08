83795abbfa87feca054236f8d08a26a2cc919303
Asbel <asbeln01@yahoo.com>
2021-07-08 16:46:24 -0500

    Putting it Together Assignment

--- assignments/react/propitup/src/components/PersonalCard.js
+++ assignments/react/propitup/src/components/PersonalCard.js
@@ -2,15 +2,26 @@
 import React, {Component} from "react";
 
 class PersonalCard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            age: props.age
+        };
+   
+    } 
+
     render(){
+        const addAge = () => {
+            this.setState({ age: this.state.age + 1 });
+        }
         return (
             <div>
-            
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={addAge}>Birthday button for {this.props.firstName} {this.props.lastName}</button>
             </div>);
+       
     }
 }
-
 export default PersonalCard;

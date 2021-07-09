c3143552da7d5dd9ed437b7a65e84d2b9ced6419
Asbel <asbeln01@yahoo.com>
2021-07-08 22:47:49 -0500

    created a functional component

--- assignments/react/big-inversion/src/App.js
+++ assignments/react/big-inversion/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import PersonalCard from './components/PersonalCard';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <PersonalCard className="personalCard" firstName="Jane" lastName="Doe" age={45} hairColor="green"/>
+        <PersonalCard className="personalCard" firstName="John" lastName="Smith" age={32} hairColor="purple"/>
+        <PersonalCard className="personalCard" firstName="Millard" lastName="Fillmore" age={50} hairColor="black"/>
+        </div>
   );
 }
 

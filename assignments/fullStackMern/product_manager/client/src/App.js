9471ecd36cbc7d7af61b744f9f9ea05fe2b96456
Asbel <asbeln01@yahoo.com>
2021-07-21 07:10:17 -0500

    added create feature on Product Manager assignment

--- assignments/fullStackMern/product_manager/client/src/App.js
+++ assignments/fullStackMern/product_manager/client/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+//import AllProduct from './components/AllProducts';
+import {Link, Router} from "@reach/router"
+import CreateProduct from './components/CreateProduct';
+//import ProductDetal from './components/ProductDetail';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>All Products</h1>
+      <Link to="/quotes/new">Add a new Product </Link>
+      <Router>
+       
+        <CreateProduct path="/quotes/new"/>
+       {/* <ProductDetail path="/quotes/info/:id"/> */}
+      </Router>
     </div>
   );
 }
---
+++ assignments/fullStackMern/product_manager/client/src/components/CreateProduct.js
@@ -0,0 +1,77 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import {navigate} from "@reach/router";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const CreateProduct=()=>{
+    const [formInfo, setFormInfo] = useState({
+        title:"",
+        price:" ",
+        description:""
+    })
+
+    const [formErrors, setFormErrors] = useState({})
+
+    //handles the changes in the input
+    const changeHandler = (e)=>{
+        setFormInfo({
+            ...formInfo,
+            [e.target.name]:e.target.value
+        })
+    }
+
+    //handles the form
+    const submitHandler = (e)=>{
+        e.preventDefault();
+        axios.post("http://localhost:8000/api/products/new", formInfo)
+            .then(res=> {
+
+                console.log("response below.......")
+                console.log(res)
+
+               
+                // if(res.data.results){
+                //     navigate("/")
+
+                //if there are errors, stay on page and set the erros
+                if(res.data.error){
+                    console.log("Please fill out the form correctly!")
+                    setFormErrors(res.data.error.errors) 
+
+                //if there arent any errosc go to home page
+                }else{
+                   
+                    navigate("/")
+                }
+
+            })
+            .catch(err=> console.log("There were errors submitting the form!", err))
+    }
+
+    return (
+        <div className = "container">
+        <h2>Product Manager.</h2>  
+        <form onSubmit={submitHandler}>
+            <div className="form-group">
+                <label>Title:</label>
+                <input onChange= {changeHandler} type="text" name="title" id="" className="form-control" />
+                {formErrors.title? <p className="text-danger">{formErrors.title.message}</p>:""}
+            </div>
+            <div className="form-group">
+                <label>Price</label>
+                <input onChange= {changeHandler} type="text" name="price" id="" className="form-control" />
+                {formErrors.price? <p className="text-danger">{formErrors.title.message}</p>: ""}
+
+            </div>
+            <div className="form-group">
+                <label>Description:</label>
+                <input  onChange= {changeHandler} type="text" name="description" id="" className="form-control" />
+            </div>
+            <input type="submit" value="Create" />
+        </form>
+      </div>
+    )
+
+}
+
+export default CreateProduct;
